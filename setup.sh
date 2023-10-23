02df4e474e228ff6eeedc7fb4869598ba8544e44
Huzaifa696 <huzaifa.rehman696@gmail.com>
2023-10-23 14:15:45 +0500

    new branch

--- setup.sh
+++ setup.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # Run the following commands as well if cargo setup is not installed
-curl https://sh.rustup.rs -sSf | sh
-source $HOME/.cargo/env
+# curl https://sh.rustup.rs -sSf | sh
+# source $HOME/.cargo/env
 
 # run the following commands if searcher-examples is not already cloned
-git clone https://github.com/jito-labs/searcher-examples.git
-cd searcher-examples
-git checkout c6d9fd1e3644498db2b089149b65bf2057ef77d9
-git submodule update --init --recursive
-git apply ../print-change.patch
+# git clone https://github.com/jito-labs/searcher-examples.git
+# cd searcher-examples
+# git checkout c6d9fd1e3644498db2b089149b65bf2057ef77d9
+# git submodule update --init --recursive
+# git apply ../print-change.patch
+# cargo build --release
+# cp ./target/release/jito-searcher-cli ~/
+# cd ..
+# sudo apt-get install -y libfontconfig1-dev
+
+sh -c "$(curl -sSfL https://release.solana.com/v1.17.3/install)"
 cargo build --release
-cp ./target/release/jito-searcher-cli ~/
-cd ..
-sudo apt-get install -y libfontconfig1-dev
-cargo build --release
+
